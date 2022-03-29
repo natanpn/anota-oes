812023dad3d3913d0469bc0e6067ad2f593b3a01
natanpn <natanpay512@gmail.com>
2022-03-28 22:41:05 -0300

    adicionando horario para as notas

--- js/add.js
+++ js/add.js
@@ -2,6 +2,14 @@ $(function(){
     var el = `<div class="box-note"><textarea placeholder="Sua anotação..."></textarea></div>`;
     $('.add-note').click(function(){
         $('.note').hide().fadeIn(500).append(el);
+
+        var textArea = $('.box-note').last().find('textarea');
+        var date = new Date();
+        var hh = date.getHours();
+        var mm = date.getMinutes();
+
+        var finalTime = hh+":"+mm;
+        textArea.html("Nova anotação em: "+finalTime);
     })
 })
 
